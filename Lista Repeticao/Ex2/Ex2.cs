465cf135a5b2c6609caea64a702ca87db6ae79b2
Nathan Ferreira Borges <nborges@ciandt.com>
2020-02-22 22:05:38 -0300

    Ex2 da lista de repeticao concluido

---
+++ Lista Repeticao/Ex2/Ex2.cs
@@ -0,0 +1,11 @@
+using System;
+/* Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, 
+mostrando uma mensagem de erro e voltando a pedir as informações.*/
+
+public class ex2
+{
+    public int Ex2(int soma1, int soma2)
+    {
+        return soma1 + soma2;
+    }
+}
