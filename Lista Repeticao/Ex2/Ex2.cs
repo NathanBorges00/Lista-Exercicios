081639bebf9820850db7718d00a14f6a7be18837
Nathan Ferreira Borges <nborges@ciandt.com>
2020-02-24 01:13:50 -0300

    Ex2 da lista de repetição corrigido e finalizado

--- Lista Repeticao/Ex2/Ex2.cs
+++ Lista Repeticao/Ex2/Ex2.cs
@@ -2,10 +2,25 @@ using System;
 /* Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, 
 mostrando uma mensagem de erro e voltando a pedir as informações.*/
 
-public class ex2
+public class Ex2
 {
-    public int Ex2(int soma1, int soma2)
+    public void ex2 ()
     {
-        return soma1 + soma2;
+        string nome;
+        string senha;
+
+        Console.WriteLine("Insira seu nome: ");
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Insira sua senha: ");
+        senha = Console.ReadLine();
+
+        while(nome.Equals(senha))
+        {
+            Console.WriteLine("Digite uma senha diferente do nome de usuario!!");
+            senha = Console.ReadLine();
+        }
+
+        Console.ReadKey();
     }
 }
--- Lista Repeticao/Ex2/Teste.cs
+++ Lista Repeticao/Ex2/Teste.cs
@@ -4,16 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Coloque um numero a ser somado: ");
-            int soma1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Coloque outro numero a ser somado: ");
-            int soma2 = Convert.ToInt32(Console.ReadLine());
-
-            ex2 soma = new ex2();
-            Console.WriteLine("O resultado foi: " + soma.Ex2(soma1,soma2));
-            
-
-            Console.ReadKey();
+            Ex2 nome = new Ex2();
+            nome.ex2();
         }
     }
